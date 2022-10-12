a27bef49f7abe5dd0424360e0d15de6663f8b54a
Mostafa <70130828+MostafaHeidari@users.noreply.github.com>
2022-10-12 12:49:10 +0200

    Onion architecture set up

---
+++ APIBoxFactory/Controllers/WeatherForecastController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BoxFactory.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class WeatherForecastController : ControllerBase
+{
+ 
+}
+
