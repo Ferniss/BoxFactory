f3d4455fabc2586e42f0c8d4a7e707a4e884cb2c
Ferniss <mathiasf2000@hotmail.com>
2022-10-12 13:29:22 +0200

    ProductsService with get and rebuild

--- Application/Class1.cs
+++ Application/Class1.cs
@@ -1,5 +1,39 @@
-﻿namespace Application;
+﻿using Application.Interfaces;
+using Domain;
 
-public class Class1
+namespace Application;
+
+public class ProductService : IProductService
 {
+    private readonly IProductRepository _productRepository;
+    private readonly IValidator<PostProductDTO> _postValidator;
+    private readonly IValidator<Product> _productValidator;
+    private readonly IMapper _mapper;
+
+    public ProductService(
+        IProductRepository repository,
+        IValidator<PostProductDTO> postValidator,
+        IValidator<Product> productValidator,
+        IMapper mapper)
+    {
+        _mapper = mapper;
+        _postValidator = postValidator;
+        _productValidator = productValidator;
+        _productRepository = repository;
+    }
+
+    public List<Product> GetAllProducts()
+    {
+        return _productRepository.GetAllProducts();
+    }
+
+    public Product GetProductById(int id)
+    {
+        return _productRepository.GetProductById(id);
+    }
+
+    public void RebuildDB()
+    {
+        _productRepository.RebuildDB();
+    }
 }
--- Infrastructure/ProductDbContext.cs
+++ Infrastructure/ProductDbContext.cs
@@ -13,9 +13,9 @@ public class ProductDbContext : DbContext
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
-             //modelBuilder.Entity<ProductDbContext>()
-            //.Property(prop => prop.Id)
-            //.ValueGenerateOnAdd();
+             modelBuilder.Entity<ProductDbContext>()
+            .Property(prop => prop.Id)
+            .ValueGenerateOnAdd();
     }
 
     public DbSet<Product> ProductTable { get; set; }
