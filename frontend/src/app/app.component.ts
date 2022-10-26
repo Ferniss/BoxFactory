9d9f109c497c2f0fe0748b512e5fef70f6eaddee
Mostafa <70130828+MostafaHeidari@users.noreply.github.com>
2022-10-26 12:47:09 +0200

    frontend add box and delete a box

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {faBoxOpen} from "@fortawesome/free-solid-svg-icons"
+import {faBox} from "@fortawesome/free-solid-svg-icons"
+import {NgbOffcanvas, NgbPanelChangeEvent, OffcanvasDismissReasons} from "@ng-bootstrap/ng-bootstrap";
+import {angularFontawesomeVersion} from "@fortawesome/angular-fontawesome/schematics/ng-add/versions";
+import {Observable} from "rxjs";
+import * as url from "url";
+import {customAxios, HttpService} from "../service/http.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'frontend';
+
+export class AppComponent implements OnInit {
+  productName: string = "";
+  productPrice: number = 0;
+  productDescription: string = "";
+  products: any[] = [];
+  faBoxOpen = faBoxOpen;
+
+
+  constructor(private http: HttpService) {
+
+  }
+
+  async ngOnInit() {
+    const products = await this.http.getProducts();
+    this.products = products;
+  }
+
+  async createProduct() {
+    let dto = {
+      price: this.productPrice,
+      name: this.productName,
+      description: this.productDescription,
+    }
+
+    const result = await this.http.createProduct(dto);
+    console.log(result)
+    this.products.push(result)
+  }
+
+  async deleteProduct(id: any) {
+    const product = await this.http.deleteProduct(id);
+    this.products = this.products.filter(p => p.id != product.id)
+  }
+
+  async updateBox(dto: {id: any, productName: any, productPrice: any, productDescription: any}, id:number){
+    const httpResult = await customAxios.put("")
+  }
 }
+
+
+
+
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,15 +2,34 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {FormsModule} from "@angular/forms";
+import {MatInputModule} from "@angular/material/input";
+import {MatButton, MatButtonModule} from "@angular/material/button";
+import {MatCardModule} from "@angular/material/card";
+import {MatSnackBar} from "@angular/material/snack-bar";
+import {Overlay} from "@angular/cdk/overlay";
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    FormsModule,
+    MatInputModule,
+    MatButtonModule,
+    MatCardModule,
+    NgbModule,
+    FontAwesomeModule
   ],
-  providers: [],
+  providers: [MatSnackBar, Overlay],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
