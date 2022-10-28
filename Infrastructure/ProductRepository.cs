51f9689b77fd6e5f6e75c33f87d3b301d46b265b
Ferniss <mathiasf2000@hotmail.com>
2022-10-28 10:17:00 +0200

    Update ProductRepository.cs

--- Infrastructure/ProductRepository.cs
+++ Infrastructure/ProductRepository.cs
@@ -5,19 +5,22 @@ namespace Infrastructure;
 
 public class ProductRepository : IProductRepository
 {
+    //instance of ProductDbContext called _context
+    private readonly ProductDbContext _context;
 
-    private readonly ProductDbContext _context; 
-    
+    // ProductRepository constrcutor with ProductDbContext as argument
     public ProductRepository(ProductDbContext context)
     {
         _context = context;
     }
-    
+
+    // can get all products there is
     public List<Product> GetAllProducts()
     {
         return _context.ProductTable.ToList();
     }
 
+    // can create/post a product
     public Product CreateNewProduct(Product product)
     {
         _context.ProductTable.Add(product);
@@ -25,17 +28,20 @@ public class ProductRepository : IProductRepository
         return product;
     }
 
+    // can get a product by id
     public Product GetProductById(int id)
     {
         return _context.ProductTable.Find(id) ?? throw new KeyNotFoundException();
     }
 
+    // can build or rebuilding db
     public void RebuildDB()
     {
         _context.Database.EnsureDeleted();
         _context.Database.EnsureCreated();
     }
-    
+
+    // can put/update a product
     public Product PutProduct(Product product)
     {
         _context.ProductTable.Update(product);
@@ -43,6 +49,7 @@ public class ProductRepository : IProductRepository
         return product;
     }
 
+    // can Patch/update a product
     public Product PatchProduct(Product product)
     {
         _context.ProductTable.Update(product);
@@ -50,7 +57,7 @@ public class ProductRepository : IProductRepository
         return product;
     }
 
-
+    // can Delete a product
     public Product DeleteProduct(int id)
     {
         var productToDelete = _context.ProductTable.Find(id) ?? throw new KeyNotFoundException();
