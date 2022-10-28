a0639074725fa50baed1b42b676ede7301d854ad
Ferniss <mathiasf2000@hotmail.com>
2022-10-28 10:24:19 +0200

    Update ProductDbContext.cs

--- Infrastructure/ProductDbContext.cs
+++ Infrastructure/ProductDbContext.cs
@@ -4,19 +4,24 @@ using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace Infrastructure;
 
+// ProductDbContext using DbContext
 public class ProductDbContext : DbContext
 {
+    // ProductDbContext constructor using ProductDbContext with base
     public ProductDbContext(DbContextOptions<ProductDbContext> opts) :base(opts)
     {
 
     }
 
+    // creating model
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        //auto crements id model
                 modelBuilder.Entity<Product>()
             .Property(p => p.Id)
             .ValueGeneratedOnAdd();
     }
 
+    // setting product table that holds products
     public DbSet<Product> ProductTable { get; set; }
 }
